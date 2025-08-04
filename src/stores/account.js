f93c30ded55969413f290922450012a8cdef1534
iddadda <iddadda241@gmail.com>
2025-08-04 10:56:50 +0900

    피니아 설정

--- src/stores/account.js
+++ src/stores/account.js
@@ -1,12 +1,24 @@
 import { reactive, computed } from "vue";
 import { defineStore } from "pinia";
 
-export const useAccountStore = defineStore("account", () => {
-  const state = reactive({
-    checked: false,
-    loggedIn: false,
-  });
-  const setChecked = (val) => (state.checked = val);
-  const setLoggedIn = (val) => (state.loggedIn = val);
-  return { state, setChecked, setLoggedIn };
-});
+export const useAccountStore = defineStore(
+  "account",
+  () => {
+    const state = reactive({
+      signedUser: null,
+      isSigned: false,
+    });
+
+    const setSignedUser = (val) => {
+      state.signedUser = val;
+      state.isSigned = true;
+    };
+
+    const logout = () => {
+      state.signedUser = null;
+      state.isSigned = false;
+    };
+    return { state, setSignedUser, logout };
+  },
+  { persist: true }
+);
